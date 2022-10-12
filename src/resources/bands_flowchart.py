94bd6f973f51ee176d2b6a93bc102502576c8965
Carlos Arranz Sánchez <carranz998@gmail.com>
2022-10-12 09:45:28 +0200

    Implement '/bandsFlowchart' endpoint

--- src/resources/bands_flowchart.py
+++ src/resources/bands_flowchart.py
@@ -1,7 +1,31 @@
+import networkx as nx
+from data_scraping.similar_bands_ids_scraper import SimilarBandsIdsScraper
+from flask import request
 from flask_restful import Resource
+from utils.bfs import bfs
 
 
 class BandsFlowchart(Resource):
     def get(self):
-        return {'band': 'flowchart'}
-            
+        response = request.get_json()
+        if not response:
+            return {
+                'data': [],
+                'code': 400
+            }
+
+        source_band_id = response['source_band_id']
+        target_band_id = response['target_band_id']
+
+        G_similar_bands = nx.Graph()
+        similar_bands_scraper = SimilarBandsIdsScraper().scrap
+
+        bfs(G_similar_bands, source_band_id, target_band_id, similar_bands_scraper)
+        bands_flowchart_ids = nx.dijkstra_path(
+            G_similar_bands, source_band_id, target_band_id
+        )
+        
+        return {
+            'data': bands_flowchart_ids,
+            'code': 200
+        }
