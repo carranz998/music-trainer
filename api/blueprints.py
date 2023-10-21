e9ab7def88615a3f7a7c666d63bf75ffab62aecf
carranz998 <carranz998@gmail.com>
2023-10-21 09:20:09 +0200

    movements

--- api/blueprints.py
+++ api/blueprints.py
@@ -1,13 +1,7 @@
 import flask
 
 albums_blueprint = flask.Blueprint(
-    'albums',
-    __name__,
-    url_prefix='/albums'
-)
+    'albums', __name__, url_prefix='/albums')
 
 flowcharts_blueprint = flask.Blueprint(
-    'flowcharts',
-    __name__,
-    url_prefix='/flowcharts'
-)
+    'flowcharts', __name__, url_prefix='/flowcharts')
