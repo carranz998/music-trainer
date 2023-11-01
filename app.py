6ad904ccf2c321eb8d8fb5dc95f942b33ade59c0
carranz998 <carranz998@gmail.com>
2023-11-01 20:57:30 +0100

    abstract service routing

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
+from typing import Any
+
 import flask
 
 from api.services import album_tracks_names, artist_to_artist
 
+
+def __create_url_rule_config(view_func: Any) -> tuple[str, str, Any]:
+    rule = f'/{view_func.__name__}'
+    endpoint = f'{view_func.__name__}'
+
+    return rule, endpoint, view_func
+
+
+def __create_blueprint_configs(url_prefix_name: str) -> dict[str, str]:
+    return {
+        'name': url_prefix_name,
+        'import_name': __name__,
+        'url_prefix': f'/{url_prefix_name}'
+    }
+
+
 app = flask.Flask(__name__)
 
-albums_blueprint = flask.Blueprint(
-    'albums', __name__, url_prefix='/albums'
-)
+services_per_url_prefix = {
+    'albums': [album_tracks_names],
+    'flowcharts': [artist_to_artist]
+}
 
-flowcharts_blueprint = flask.Blueprint(
-    'flowcharts', __name__, url_prefix='/flowcharts'
-)
+for url_prefix, services in services_per_url_prefix.items():
+    blueprint_configs = __create_blueprint_configs(url_prefix)
+    blueprint = flask.Blueprint(**blueprint_configs)
 
-albums_blueprint.add_url_rule(
-    '/album_tracks_names', 'album_tracks_names', album_tracks_names
-)
+    for service in services:
+        rule, endpoint, view_func = __create_url_rule_config(service)
+        blueprint.add_url_rule(rule, endpoint, view_func)
 
-flowcharts_blueprint.add_url_rule(
-    '/artist_to_artist', 'artist_to_artist', artist_to_artist
-)
+    app.register_blueprint(blueprint)
 
-app.register_blueprint(albums_blueprint)
-app.register_blueprint(flowcharts_blueprint)
 
 if __name__ == '__main__':
     app.run()
