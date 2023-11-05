df6ece50705119dfe106f95eed3f168b5659bbec
carranz998 <carranz998@gmail.com>
2023-11-05 08:19:42 +0100

    renamings

--- spotify_api_facade/services/artist_name.py
+++ spotify_api_facade/services/artist_name.py
@@ -5,11 +5,11 @@ from spotify_api_facade.utils.spotify_api_request_handler import \
 
 
 class __Artist_Name(Spotify_Api_Request_Handler):
-    def __init__(self, artist_id: str) -> None:
-        self.artist_id = artist_id
+    def __init__(self, spotify_artist_id: str) -> None:
+        self.spotify_artist_id = spotify_artist_id
 
     def _set_url(self) -> str:
-        return f'https://api.spotify.com/v1/artists/{self.artist_id}'
+        return f'https://api.spotify.com/v1/artists/{self.spotify_artist_id}'
 
     def _set_http_method(self) -> str:
         return 'get'
